343eed60aa8caf2b3077f748475d8c4288ee697e
elifo191 <elifo191@student.liu.se>
2025-04-04 20:53:10 +0200

    test

--- src/project.vhdl
+++ src/project.vhdl
@@ -2,18 +2,13 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity tt_um_example is
+entity ps2_reciever is
     port (
-        ui_in   : in  std_logic_vector(7 downto 0);
-        uo_out  : out std_logic_vector(7 downto 0);
-        uio_in  : in  std_logic_vector(7 downto 0);
-        uio_out : out std_logic_vector(7 downto 0);
-        uio_oe  : out std_logic_vector(7 downto 0);
-        ena     : in  std_logic;
-        clk     : in  std_logic;
-        rst_n   : in  std_logic
+        clk     :   in  std_logic;
+        d_in    :   in  std_logic;
+        d_out   :   out std_logic_vector(10 downto 0)
     );
-end tt_um_example;
+end ps2_reciever;
 
 architecture Behavioral of tt_um_example is
 begin
